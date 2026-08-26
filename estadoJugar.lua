dc0cea5863eb4abb4c4fb6b26ac4d93f4fe441d9
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-08-26 15:54:25 -0300

    Clase EstadoJugar

---
+++ estadoJugar.lua
@@ -0,0 +1,7 @@
+EstadoJugar = Class { __includes = Estado }
+
+function EstadoJugar:init() end
+function EstadoJugar:ingresar() end
+function EstadoJugar:salir() end
+function EstadoJugar:actualizar(dt) end
+function EstadoJugar:dibujar() end
