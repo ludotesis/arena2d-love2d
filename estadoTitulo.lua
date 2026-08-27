7299268ba3c7fc2bc6574814438e44f59f991168
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-08-27 10:58:47 -0300

    Comportamiento de ingreso a estados

--- estadoTitulo.lua
+++ estadoTitulo.lua
@@ -1,11 +1,15 @@
 EstadoTitulo = Class { __includes = Estado }
 
-function EstadoTitulo:init() end
-function EstadoTitulo:ingresar() end
+function EstadoTitulo:init()
+    self.titulo = "NUEVA ARENA"
+end
+function EstadoTitulo:ingresar()
+    self.titulo = "LUDOTESIS"
+end
 function EstadoTitulo:salir() end
 function EstadoTitulo:actualizar(dt) end
 
 function EstadoTitulo:dibujar()
-    love.graphics.printf('ARENA 2D', 0, 64, ventana.ancho, 'center')
+    love.graphics.printf(self.titulo, 0, 64, ventana.ancho, 'center')
     love.graphics.printf('Presionar Enter', 0, 100, ventana.ancho, 'center')
 end
